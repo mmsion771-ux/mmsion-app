fff4a9c1b28f6e974b121f9aca2c29a12d09771b
mmsion771-ux <mmsion771@gmail.com>
2026-02-08 15:25:00 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "mmsion-pwa-v5";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/services.html",
+  "/form.html",
+  "/privacy.html",
+  "/manifest.webmanifest",
+  "/assets/logo.png",
+  "/assets/bg.png",
+  "/assets/icon-192.png",
+  "/assets/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
+
